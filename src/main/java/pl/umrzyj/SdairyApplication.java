04f6e71683a4226784f741b22f156c343cb90a69
askorska <askorska@pgs-soft.com>
2016-04-27 00:16:35 +0200

    first commit - setup

---
+++ src/main/java/pl/umrzyj/SdairyApplication.java
@@ -0,0 +1,12 @@
+package pl.umrzyj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdairyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdairyApplication.class, args);
+	}
+}
