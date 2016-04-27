3292abd5f170edcb73a14d6c1ff8be72f0905d3f
Agata Skorska (PGS Software) <askorska@pgs-soft.com>
2016-04-27 16:54:47 +0200

    more rests

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -3,23 +3,38 @@ package hello;
 /**
  * Created by askorska on 2016-04-27.
  */
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class HelloController {
+
+
+    List<String> przedmioty = new ArrayList<>();
 
     int iloscDzieci = 0;
 
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "Dzienniczek ucznia";
     }
 
 
     @RequestMapping("/przedmioty")
-    public String przedmioty() {
-        return "Tu będą przedmioty " + iloscDzieci ++;
+    public List<String> przedmioty() {
+        return przedmioty;
     }
+
+
+    @RequestMapping(path = "/addPrzedmioty", method = RequestMethod.POST)
+    public int dodajPrzedmiot(@RequestParam String przedmiot) {
+        przedmioty.add(przedmiot);
+        return przedmioty.size();
+    }
+
+
+
 
 }
